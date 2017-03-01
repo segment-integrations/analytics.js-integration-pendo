01dd0321bff0cfa423a84bd253fddbe2bed32523
Michael Dellanoce <michael.dellanoce@gmail.com>
2017-03-01 12:50:45 -0500

    Release 1.1.0
    * Update location of pendo agent

--- test/index.test.js
+++ test/index.test.js
@@ -10,7 +10,7 @@ describe('Pendo', function() {
   var analytics;
   var pendo;
   var options = {
-    apiKey: 'an-id-to-test-with'
+    apiKey: 'test-key-for-segment-integration'
   };
 
   beforeEach(function() {
